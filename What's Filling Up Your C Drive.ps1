703b05b7bb6f536109bb84409a63c3648eeb4e7e
Troubleshoot Terminal <troubleshootterminal@gmail.com>
2026-08-14 16:41:59 +0100

    Add files via upload

---
+++ What's Filling Up Your C Drive.ps1
@@ -0,0 +1,3 @@
+﻿Get-ChildItem -Path C:\ -Recurse -File -ErrorAction SilentlyContinue |
+  Sort-Object Length -Descending |
+  Select-Object -First 10 FullName, @{N="Size (GB)";E={[math]::Round($_.Length/1GB, 2)}}
